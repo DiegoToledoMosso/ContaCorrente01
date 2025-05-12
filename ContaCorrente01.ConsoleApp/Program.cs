840768d90c0b3cf5d67f21f43654011f6d3c0f85
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-11 22:12:22 -0300

    Add project files.

---
+++ ContaCorrente01.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContaCorrente01.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

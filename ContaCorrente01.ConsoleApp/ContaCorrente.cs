29591b880cae55d29d2cbbd103532c570992ad01
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-11 22:37:10 -0300

    Finalizado a primeira versão teste, refatorada e remoção de todos os comentários

--- ContaCorrente01.ConsoleApp/ContaCorrente.cs
+++ ContaCorrente01.ConsoleApp/ContaCorrente.cs
@@ -1,93 +1,70 @@
 ﻿
+using ContaCorrente01.ConsoleApp;
 
-namespace ContaCorrente01.ConsoleApp
+namespace ContaCorrente.ConsoleApp
 {
+
     public class ContaCorrente
     {
-
-        //Aqui iremos colocar todas os metodos e declarar suas funçoes.
-
-
-        public double saldo;
+        
         public int numero;
-        public int limite;
-
+        public decimal saldo;
+        public decimal limite;
         public Movimentacao[] movimentacoes;
+        int contadorMovimentacoes = 0;
 
-        public void Sacar(double quantia)
+        
+        public void Sacar(decimal quantia)
         {
-            double novoSaldo = saldo - quantia;
+            if (quantia <= saldo + limite)
+            {
+                saldo -= quantia;
 
-            saldo = novoSaldo;
+                Movimentacao novaMovimentacao = new Movimentacao();
+                novaMovimentacao.valor = quantia;
+                novaMovimentacao.tipo = "Débito";
 
-            Movimentacao movimentacao = new Movimentacao();
-            movimentacao.valor = quantia;
-            movimentacao.tipo = "Débito";
-            movimentacao.descricao = "Débito de R$" + quantia + " reais";
-
-            int posicaoVazia = PegaPosicaoVazia();
-
-            movimentacoes[posicaoVazia] = movimentacao;
-
+                movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+                contadorMovimentacoes++;
+            }
         }
 
-        public void Depositar(double quantia)
+        public void Depositar(decimal quantia)
         {
-            double novoSaldo = saldo + quantia;
+            saldo += quantia;
 
-            saldo = novoSaldo;
+            Movimentacao novaMovimentacao = new Movimentacao();
+            novaMovimentacao.valor = quantia;
+            novaMovimentacao.tipo = "Crédito";
 
-            Movimentacao movimentacao = new Movimentacao();
-            movimentacao.valor = quantia;
-            movimentacao.tipo = "Crédito";
-            movimentacao.descricao = "Crédito de R$" + quantia + " reais";
-
-            int posicaoVazia = PegaPosicaoVazia();
-
-            movimentacoes[posicaoVazia] = movimentacao;
-
+            movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+            contadorMovimentacoes++;
         }
 
-        public void TransferirPara(ContaCorrente contaDestino, double quantia)
+        public void TransferirPara(ContaCorrente destinatario, decimal quantia)
         {
+            destinatario.Depositar(quantia);
+
             this.Sacar(quantia);
-
-            contaDestino.Depositar(quantia);
         }
 
         public void ExibirExtrato()
         {
-            Console.WriteLine("Numero da conta {0}", numero);
+            Console.WriteLine("Extrato da Conta #" + this.numero);
 
-            Console.WriteLine("Movimentações: ");
+            Console.WriteLine("Saldo: " + this.saldo);
 
-            foreach (Movimentacao movimentacao in movimentacoes)
-            {
-                if (movimentacao != null)
-                    Console.WriteLine(movimentacao.descricao);
-            }
+            Console.WriteLine();
 
-            Console.WriteLine("Saldo atual: {0}", saldo + limite);
-        }
-
-        public int PegaPosicaoVazia()
-        {
             for (int i = 0; i < movimentacoes.Length; i++)
             {
-                if (movimentacoes[i] == null)
-                    return i;
+                Movimentacao movimentacaoAtual = movimentacoes[i];
+
+                if (movimentacaoAtual != null)
+                {
+                    Console.WriteLine(movimentacaoAtual.LerMovimentacao());
+                }
             }
-
-            return -1;
         }
-
-
-
-
-
-
-
-
-
     }
 }
--- ContaCorrente01.ConsoleApp/Program.cs
+++ ContaCorrente01.ConsoleApp/Program.cs
@@ -1,65 +1,71 @@
 ﻿namespace ContaCorrente01.ConsoleApp
 {
-    internal class Program
+
+    namespace ContaCorrente.ConsoleApp
     {
-        /*Versão 01
-         * Criar uma conta corrente com as seguintes funcionalidades:
-         * Saldo = sacar depositar
-         * Numero da conta
-         * limete da conta
-         * movimentação
-         * transferencia entre contas
-         */
 
-        static void Main(string[] args)
+        public class ContaCorrente
         {
-            Console.WriteLine("Bem vindo a sua conta corrente.");
-
-            //Iremos trabalhar agora na conta 01
-
-            //Criar uma conta corrente
-            ContaCorrente conta1 = new ContaCorrente();
-            //saldo
-            conta1.saldo = 1000;
-            //numero da conta
-            conta1.numero = 12;
-            //limete da conta
-            conta1.limete = 0;
-            //movimentação contendo apenas 10
-            conta1.movimentacoes = new Movimentacao[10];
-
-            //Iremos inserir os comando de sacar e depositar em metodos
-
-            conta1.Sacar(200);
-
-            conta1.Depositar(300);
-
-            conta1.Depositar(500);
-
-            conta1.Sacar(200);
-
-            //Iremos trabalhar agora na conta 02
-
-            ContaCorrente conta2 = new ContaCorrente();
-            conta2.saldo = 300;
-            conta2.numero = 13;
-            conta2.limite = 0;
-            conta2.movimentacoes = new Movimentacao[10];
-
-            //Comando transferir
-            conta1.TransferirPara(conta2, 400);
+            
+            public int numero;
+            public decimal saldo;
+            public decimal limite;
+            public Movimentacao[] movimentacoes;
+            int contadorMovimentacoes = 0;
 
             
-            conta1.ExibirExtrato();
+            public void Sacar(decimal quantia)
+            {
+                if (quantia <= saldo + limite)
+                {
+                    saldo -= quantia;
 
+                    Movimentacao novaMovimentacao = new Movimentacao();
+                    novaMovimentacao.valor = quantia;
+                    novaMovimentacao.tipo = "Débito";
 
+                    movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+                    contadorMovimentacoes++;
+                }
+            }
 
+            public void Depositar(decimal quantia)
+            {
+                saldo += quantia;
 
+                Movimentacao novaMovimentacao = new Movimentacao();
+                novaMovimentacao.valor = quantia;
+                novaMovimentacao.tipo = "Crédito";
 
+                movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+                contadorMovimentacoes++;
+            }
 
+            public void TransferirPara(ContaCorrente destinatario, decimal quantia)
+            {
+                destinatario.Depositar(quantia);
 
+                this.Sacar(quantia);
+            }
 
+            public void ExibirExtrato()
+            {
+                Console.WriteLine("Extrato da Conta #" + this.numero);
 
+                Console.WriteLine("Saldo: " + this.saldo);
+
+                Console.WriteLine();
+
+                for (int i = 0; i < movimentacoes.Length; i++)
+                {
+                    Movimentacao movimentacaoAtual = movimentacoes[i];
+
+                    if (movimentacaoAtual != null)
+                    {
+                        Console.WriteLine(movimentacaoAtual.LerMovimentacao());
+                    }
+                }
+            }
         }
     }
 }
